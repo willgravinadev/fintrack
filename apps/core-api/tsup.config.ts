64433bcc35889bf272e5a73d4bf95f10a7391fe9
W I L L <will@gravina.dev>
2025-03-31 11:20:54 -0300

    chore: add '@fintrack/db' to noExternal

--- apps/core-api/tsup.config.ts
+++ apps/core-api/tsup.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     '@fintrack/env',
     '@fintrack/logger',
     '@fintrack/token',
-    '@fintrack/utils'
+    '@fintrack/utils',
+    '@fintrack/db'
   ]
 })
--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -5,5 +5,7 @@ export default defineConfig({
   dts: true,
   format: ['esm'],
   target: 'esnext',
-  clean: true
+  clean: true,
+  splitting: true,
+  sourcemap: true
 })
